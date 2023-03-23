80c37bac8d926ad12a1d82e68d5722095aa44b54
JykLouie <jykuang@bu.edu>
2023-03-23 15:53:32 -0400

    Router ver 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import MusicList from './components/MusicList/MusicList';
+import Navbar from './components/Navbar/Navbar';
+import Profile from './components/Profile/Profile';
+import RecipeList from './components/RecipeList/RecipeList';
+import SearchBar from './components/SearchBar/SearchBar'
+import LoginPage from './components/Login/LoginPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar />
+        <header className='App-header'>
+          <Routes>
+            <Route exact path='/' element={<SearchBar/>} />
+
+            <Route exact path='/result'>
+              {[<MusicList/>,<RecipeList/>]}
+            </Route>
+
+            <Route exact path='/Profile' element={<Profile/>} />
+            <Route exact path='/LoginPage' element={<LoginPage/>} />
+
+          </Routes>
+        </header>
+      </BrowserRouter>
     </div>
   );
 }

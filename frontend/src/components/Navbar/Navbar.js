e32200deaa47e49f3eef6ce1d4e08fb63ffe49b3
Rong Chen <rongc0723@gmail.com>
2023-03-24 21:41:37 -0400

    starting navbar

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -7,8 +7,8 @@ export default function Navbar() {
   return (
     <div className='navbar'>
         <nav>
-            <NavLink className='brand' to='/'>
-            </NavLink>
+            <NavLink className='brand' to='/'>Sfoodify</NavLink>
+            <NavLink className='brand' to='/Profile'>Profile</NavLink>
         </nav>
     </div>
   )

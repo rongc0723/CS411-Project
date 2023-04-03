6e3094302fe975666355daa878a69a60a0ad68bf
Cheng <x04010710@gmail.com>
2023-04-03 17:52:11 -0400

    add favorite button + styling

--- frontend/src/pages/MainPage/MainPage.js
+++ frontend/src/pages/MainPage/MainPage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import SearchBar from '../../components/SearchBar/SearchBar'
+import Favorite from '../../components/Favorite/Favorite'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './MainPage.css'
 
@@ -9,6 +10,7 @@ export default function MainPage() {
       <h1>Welcome to Sfoodify</h1>
       <h3>Search for recipe </h3>
       <SearchBar/>
+      <Favorite/>
     </div>
   )
 }

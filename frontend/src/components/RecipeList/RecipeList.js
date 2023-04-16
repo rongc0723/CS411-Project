b30a634dd0bef5cad078761777205a77ae9fa316
Rong Chen <rongc0723@gmail.com>
2023-04-16 13:09:53 -0400

    playlists

--- frontend/src/components/RecipeList/RecipeList.js
+++ frontend/src/components/RecipeList/RecipeList.js
@@ -7,12 +7,14 @@ import CardGroup from 'react-bootstrap/CardGroup'
 import FavoriteButton from '../../components/Favorite/Favorite'
 import { useEffect } from 'react';
 import axios from 'axios';
+import MusicList from '../MusicList/MusicList';
+import {Spotify} from 'react-spotify-embed'
 
 const RecipeList = () =>{
   const urlParams = new URLSearchParams(window.location.search);
   const objString = urlParams.get('obj');
   const selectedDish= JSON.parse(objString.slice(4));
-  console.log(selectedDish);
+  // console.log(selectedDish);
 
   const query = selectedDish.cuisines[0]
   const [songs, setSongs] = useState([]);
@@ -21,17 +23,16 @@ const RecipeList = () =>{
     const fetchSongs = async (e) => {
       try {
         const response = await axios.get(`http://localhost:4000/api/spotify?query=${query}`)
-        setSongs(response.data);
+        setSongs(response.data.playlists.items);
       } catch (error) {
         console.log(error)
       }
     }
     fetchSongs();
   }, [query])
-
-  console.log(songs);
   
   return (
+    <div className='results-page'>
     <Card style={{width: '25rem'}}>
       <Card.Img variant='top' src={selectedDish.image} sizes='(max-width: 710px) 120px,(max-width: 991px) 193px,278px'></Card.Img>
       <Card.Body>
@@ -55,6 +56,10 @@ const RecipeList = () =>{
           </Accordion.Item>))}
       </Accordion>
     </Card>
+    {songs.length > 1 && songs.map(song => (
+      <Spotify link={song.external_urls.spotify} />
+    ))}
+  </div>
   )
   
 }

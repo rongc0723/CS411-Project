7022b03f36750f3a039d9fd2f5ff16261c2208da
JykLouie <jykuang@bu.edu>
2023-04-16 21:34:11 -0400

    Music List get, styled. Recipelist restyled

--- frontend/src/components/MusicList/MusicList.js
+++ frontend/src/components/MusicList/MusicList.js
@@ -1,7 +1,54 @@
 import React from 'react'
+import { Spotify } from 'react-spotify-embed'
+import { getQuery } from '../RecipeList/RecipeList.js'
+import { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
+import './MusicList.css'
+import Card from 'react-bootstrap/Card'
 
 export default function MusicList() {
+  const query = getQuery();
+  const [songs, setSongs] = useState([]);
+  useEffect(() => {
+    const fetchSongs = async (e) => {
+      try {
+        const response = await axios.get(`http://localhost:4000/api/spotify?query=${query}`)
+        setSongs(response.data.playlists.items);
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchSongs();
+  }, [query])
   return (
-    <div>MusicList</div>
+    <div className='MusicList'>
+    <div className='MusicList'>
+      <div>
+          {songs.length > 1 && songs.slice(0,2).map(song => (
+            <div>
+                <Spotify link={song.external_urls.spotify} />
+                <br></br>
+                <br></br>
+                <br></br>
+            </div>
+
+          ))}      
+      </div>
+    </div>
+    <div className='MusicList'>
+      <div>
+          {songs.length > 1 && songs.slice(2,4).map(song => (
+            <div>
+                <Spotify link={song.external_urls.spotify} />
+                <br></br>
+                <br></br>
+                <br></br>
+                </div>
+          ))}
+      </div>
+    </div>      
+    </div>
+
   )
 }
--- frontend/src/components/RecipeList/RecipeList.js
+++ frontend/src/components/RecipeList/RecipeList.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import { useState } from 'react';
 import Accordion from 'react-bootstrap/Accordion'
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
 import CardGroup from 'react-bootstrap/CardGroup'
 import FavoriteButton from '../../components/Favorite/Favorite'
 import { useEffect } from 'react';
-import axios from 'axios';
-import MusicList from '../MusicList/MusicList';
-import {Spotify} from 'react-spotify-embed'
+import './RecipeList.css'
 
 const RecipeList = () =>{
   const urlParams = new URLSearchParams(window.location.search);
@@ -17,23 +14,11 @@ const RecipeList = () =>{
   // console.log(selectedDish);
 
   const query = selectedDish.cuisines[0]
-  const [songs, setSongs] = useState([]);
-
-  useEffect(() => {
-    const fetchSongs = async (e) => {
-      try {
-        const response = await axios.get(`http://localhost:4000/api/spotify?query=${query}`)
-        setSongs(response.data.playlists.items);
-      } catch (error) {
-        console.log(error)
-      }
-    }
-    fetchSongs();
-  }, [query])
   
   return (
-    <div className='results-page'>
-    <Card style={{width: '25rem'}}>
+    <div className='RecipeList'>
+    <CardGroup>
+    <Card style={{width: '20rem'}}>
       <Card.Img variant='top' src={selectedDish.image} sizes='(max-width: 710px) 120px,(max-width: 991px) 193px,278px'></Card.Img>
       <Card.Body>
         <Card.Title>
@@ -42,11 +27,11 @@ const RecipeList = () =>{
         </Card.Title>
       </Card.Body>
       <Card.Text>
-        Culture: {selectedDish.cuisines[0]}.
+        Culture: {selectedDish.cuisines[0]}
       </Card.Text>
       <Accordion>
-        {selectedDish.analyzedInstructions[0].steps.map(instructions => (
-          <Accordion.Item>
+        {selectedDish.analyzedInstructions[0].steps.slice(0,8).map(instructions => (
+          <Accordion.Item eventKey={instructions.number}>
             <Accordion.Header>
               Step {instructions.number}
             </Accordion.Header>
@@ -56,13 +41,35 @@ const RecipeList = () =>{
           </Accordion.Item>))}
       </Accordion>
     </Card>
-    {songs.length > 1 && songs.map(song => (
-      <Spotify link={song.external_urls.spotify} />
-    ))}
+    <Card style={{width: '20rem'}}>
+      <Accordion>
+        {selectedDish.analyzedInstructions[0].steps.slice(8).map(instructions => (
+          <Accordion.Item eventKey={instructions.number}>
+            <Accordion.Header>
+              Step {instructions.number}
+            </Accordion.Header>
+            <Accordion.Body>
+              {instructions.step}
+            </Accordion.Body>
+          </Accordion.Item>))}
+      </Accordion>      
+    </Card>      
+    </CardGroup>
+
+
   </div>
   )
   
 }
 
+const getQuery = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const objString = urlParams.get('obj');
+  const selectedDish= JSON.parse(objString.slice(4));
+  const query = selectedDish.cuisines[0]
+  return query;
+}
+
 
 export default RecipeList;
+export {getQuery};
